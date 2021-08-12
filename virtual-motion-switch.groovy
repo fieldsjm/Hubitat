6cf3a8b388c11786928cac961595562074b5c7e1
fieldsjm <39744574+fieldsjm@users.noreply.github.com>
2021-08-12 15:12:39 -0400

    Create virtual-motion-switch.groovy

---
+++ virtual-motion-switch.groovy
@@ -0,0 +1,48 @@
+/*
+ * Virtual Motion with Switch (Independent)
+ *
+ *  https://raw.githubusercontent.com/ogiewon/Hubitat/master/Drivers/virtual-presence-switch.src/virtual-presence-switch.groovy
+ *
+ *  Copyright 2021 Jonathan Fields
+ *
+ *  Licensed Virtual the Apache License, Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License. You may obtain a copy of the License at:
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
+ *  for the specific language governing permissions and limitations under the License.
+ */
+
+metadata {
+    definition (name: "Virtual Motion with Switch", namespace: "fieldsjm", author: "Jonathan Fields", importUrl: "") {
+        capability "Sensor"
+        capability "Motion"
+        capability "Switch"
+		
+        command "active"
+        command "inactive"
+        command "on"
+        command "off"
+    }   
+}
+
+def active() {
+	sendEvent(name: "motion", value: "active")
+}
+
+def inactive() {
+	sendEvent(name: "motion", value: "inactive")
+}
+
+def on() {
+    sendEvent(name: "switch", value: "on")
+}
+
+def off() {
+    sendEvent(name: "switch", value: "off")
+}
+
+def installed() {
+}
